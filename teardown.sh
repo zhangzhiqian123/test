b1764523d9628923ea5ecc5e1cf2d1a57bdf0618
zhangzhiqian123 <38027105+zhangzhiqian123@users.noreply.github.com>
2019-03-10 23:28:29 +0800

    Add files via upload

---
+++ teardown.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Exit on first error, print all commands.
+set -e
+
+rm -f ~/.hfc-key-store/*
+
+cd ../basic-network
+
+# Shut down the Docker containers for the system tests.
+docker-compose -f docker-compose.yml kill && docker-compose -f docker-compose.yml down
+if [ "$(docker ps -aq)" ]; then
+	docker rm -f $(docker ps -aq)
+fi
+
+# remove chaincode docker images
+if [ "$(docker images dev-* -q)" ]; then
+	docker rmi $(docker images dev-* -q)
+fi
+
+# Your system is now clean
